8ef053badc292703bff851bef3319068d72b4d2f
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-10-22 18:37:05 -0700

    Upgraded tf version

--- modules/domain/terraform.tf
+++ modules/domain/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.11.1"
+      version = "~> 2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.5.0"
+      version = "~> 2"
     }
   }
   required_version = "~> 1"
